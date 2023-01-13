0366446dded5d48d2ee29abfeb92d6d1190263a0
HIMMEL <bbluesky7738@gmail.com>
2023-01-13 20:44:06 +0900

    2023-01-13 20:44

--- src/test/java/org/example/RequestLineTest.java
+++ src/test/java/org/example/RequestLineTest.java
@@ -8,8 +8,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RequestLineTest {
     @Test
     void create() {
-        RequestLine requstLine = new RequestLine("GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1");
+        RequestLine requestLine = new RequestLine("GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1");
 
-        assertThat(requstLine).isNotNull();
+        assertThat(requestLine).isNotNull();
+        assertThat(requestLine).isEqualTo(new RequestLine("GET", "/calculate", "operand1=11&operator=*&operand2=55"));
     }
 }
