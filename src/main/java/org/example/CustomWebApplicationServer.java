ebd3cfe33d6fa3e4e2ee2b590ff66d519140157d
HIMMEL <bbluesky7738@gmail.com>
2023-01-17 08:28:44 +0900

    2023-01-17 08:28

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -6,9 +6,13 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -25,7 +29,10 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomeWebApplicationServer] client connected!");
 
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                /*Step3 - Thread Pool 사용*/
+                executorService.execute(new ClientRequestHandler(clientSocket));
+/*//                Step2 - 사용자 요청이 들어올 때마다 Thread 를 새로 생성해서 사용자 요청을 처리하도록 한다.
+                new Thread(new ClientRequestHandler(clientSocket)).start(); */
             }
         }
     }
